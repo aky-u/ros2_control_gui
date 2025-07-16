826805cb9d91b710b5b4b94719a9cf48dcc25fcc
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-07-16 07:43:11 +0000

    Add functionality to request and log ROS 2 controller states in JointControllerGui

--- ros2_control_gui/joint_controller_gui.py
+++ ros2_control_gui/joint_controller_gui.py
@@ -2,9 +2,12 @@ import sys
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 from PySide6.QtCore import QThread
 
+from controller_manager_msgs.srv import ListControllers
+from controller_manager_msgs.srv import SwitchController
 
 # ROS 2 Node for joint controller GUI
 class JointControllerGui(Node):
@@ -12,11 +15,35 @@ class JointControllerGui(Node):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
 
+        # Create Client to get ros2 controllers state
+        self.list_controllers_client = \
+            self.create_client(ListControllers, '/controller_manager/list_controllers')
+        
+        self.controllers_list = []
+
     def publish_message(self, msg: str):
         msg_obj = String()
         msg_obj.data = msg
         self.publisher_.publish(msg_obj)
 
+    def request_controllers(self):
+        if not self.list_controllers_client.service_is_ready():
+            self.get_logger().info('Waiting for /list_controllers service...')
+            return
+
+        request = ListControllers.Request()
+        future = self.list_controllers_client.call_async(request)
+        future.add_done_callback(self.on_controllers_response)
+
+    def on_controllers_response(self, future):
+        try:
+            response = future.result()
+            self.controllers_list.clear()
+            for controller in response.controller:
+                self.controllers_list.append(controller)
+                self.get_logger().info(f"Controller: {controller.name}, State: {controller.state}")
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
 
 # Thread to run ROS spin in the background
 class RosSpinThread(QThread):
@@ -55,7 +82,8 @@ class MainWindow(QWidget):
 
     def on_button_released(self, radian):
         print(f"Released with radian: {radian}")
-        self.ros_node.publish_message(f"Released with radian {radian}")
+        # self.ros_node.publish_message(f"Released with radian {radian}")
+        self.ros_node.request_controllers()
 
 
 # Main function to run the application
